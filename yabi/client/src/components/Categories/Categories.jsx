836a682075221716534db1ee61d2e7a97f46a0b8
eliaargirova <145685779+eliaargirova@users.noreply.github.com>
2024-10-10 15:14:34 +0100

    Added hover to the category page and created the contact section

--- yabi/client/src/components/Categories/Categories.jsx
+++ yabi/client/src/components/Categories/Categories.jsx
@@ -1,60 +1,84 @@
-import React from 'react'
-import "./Categories.scss"
-import {Link} from 'react-router-dom'
-
+import React from "react";
+import "./Categories.scss";
+import { Link } from "react-router-dom";
 
 const Categories = () => {
   return (
-    <div className='categories'>
-        <div className="col">
-            <div className="row">
-                <img src="public/images/card-image-3.jpg"/>
-                <button>
-                    <Link className="link" to="/products/1">Вода</Link>
-                </button>
-            </div>
-            <div className="row">
-            <img src="public/images/holistic.jpg"/>
-            <button>
-                    <Link className="link" to="/products/2">Огън</Link>
-                </button>
-            </div>
-        </div>
-        <div className="col">
-            <div className="row">
-            <img src="public/images/forest.jpg"/>
-            <button>
-                    <Link className="link" to="/products/2">Огън</Link>
-                </button>
-            </div>
-        </div>
-        <div className="col col-l">
+    <div className="categories">
+      <div className="col">
         <div className="row">
-        <div className="col">
+          <div className="image-container">
+            <img src="public/images/card-image-3.jpg" alt="Category 1" />
+          </div>
+          <button>
+            <Link className="link" to="/products/1">
+              Жени
+            </Link>
+          </button>
+        </div>
+        <div className="row">
+          <div className="image-container">
+            <img src="public/images/holistic.jpg" alt="Category 2" />
+          </div>
+          <button>
+            <Link className="link" to="/products/2">
+              Аксесоари
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col">
+        <div className="row">
+          <div className="image-container">
+            <img src="public/images/forest.jpg" alt="Category 3" />
+          </div>
+          <button>
+            <Link className="link" to="/products/2">
+              Мъже
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
             <div className="row">
-            <img src="public/images/yoga-card.jpg"/>
-            <button>
-                    <Link className="link" to="/products/2">Огън</Link>
-                </button>
+              <div className="image-container">
+                <img src="public/images/yoga-card.jpg" alt="Category 4" />
+              </div>
+              <button>
+                <Link className="link" to="/products/2">
+                  Постелки
+                </Link>
+              </button>
             </div>
-        </div>
-        <div className="col">
+          </div>
+          <div className="col">
             <div className="row">
-            <img src="public/images/fire.jpg"/>
-            <button>
-                    <Link className="link" to="/products/2">Огън</Link>
-                </button>
+              <div className="image-container">
+                <img src="public/images/fire.jpg" alt="Category 5" />
+              </div>
+              <button>
+                <Link className="link" to="/products/2">
+                  Книги
+                </Link>
+              </button>
             </div>
+          </div>
         </div>
+        <div className="row">
+          <div className="image-container">
+            <img src="public/images/air.jpg" alt="Category 6" />
+          </div>
+          <button>
+            <Link className="link" to="/products/2">
+              За дома
+            </Link>
+          </button>
         </div>
-        <div className="row">            <img src="public/images/air.jpg"/>
-            <button>
-                    <Link className="link" to="/products/2">Огън</Link>
-                </button></div>
-        </div>
-
+      </div>
     </div>
   );
-}
+};
 
 export default Categories;
